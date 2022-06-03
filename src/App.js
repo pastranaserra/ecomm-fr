e148c9bccdacd95bff0ac8cab9e7c5fd81c4f72e
Karlo Verde <karlo.verde.1996@gmail.com>
2022-06-02 21:15:45 -0500

    feat: use mui-based placeholder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import Typography from '@mui/material/Typography';
+
 const Home = lazy(() => import('./pages/Home'));
 const NotFound = lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
     <div className="App">
-      <Suspense
-        fallback={
-          <div>
-            <h1>Loading...</h1>
-          </div>
-        }
-      >
+      <Suspense fallback={<LoadingPagePlaceholder />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
     </div>
+  );
+}
+
+function LoadingPagePlaceholder() {
+  return (
+    <Typography textAlign="center" variant="h3" component="h1" gutterBottom>
+      Loading...
+    </Typography>
   );
 }
 
