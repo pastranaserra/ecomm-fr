1a73601d7cf173dde7611eaf620339fe838ff586
Karlo Verde <karlo.verde.1996@gmail.com>
2022-06-03 20:07:03 -0500

    chore: create login method

--- src/api/auth.js
+++ src/api/auth.js
@@ -14,3 +14,17 @@ export const signUp = async ({ name, lastName, email, password }) => {
     throw e;
   }
 };
+
+export const logIn = async ({ email, password }) => {
+  try {
+    return await apiClient.post('/auth/log-in', {
+      email,
+      password,
+    });
+  } catch (e) {
+    const status = e.response?.status;
+    if (status === 401) throw new Error('Invalid email or password');
+    if (status === 500) throw new Error('Unexpected error');
+    throw e;
+  }
+};
